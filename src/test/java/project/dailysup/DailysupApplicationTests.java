aaa9335d2c1f4134a3007a4b7573d60d5cc9ca75
kh0712 <kkkh0712@naver.com>
2021-03-18 19:04:45 +0900

    test properties 추가.

--- src/test/java/project/dailysup/DailysupApplicationTests.java
+++ src/test/java/project/dailysup/DailysupApplicationTests.java
@@ -1,9 +1,16 @@
 package project.dailysup;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {"spring.config.location="
+            +"classpath:/application.yml"
+            +",classpath:/jwt.yml"}
+)
 class DailysupApplicationTests {
 
     @Test
